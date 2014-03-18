101e96bdf38c31c7ad1d812f6eefc8b589fc6f57
tony311 <tony@tonyfox.ws>
2014-03-18 16:58:26 -0400

    General cleanup

--- GatherNotify.lua
+++ GatherNotify.lua
@@ -1,24 +1,5 @@
---[[ Changelog:
-	1.0 : Initial release
-	1.1 : Added "(currently xxx)" to end of "Requires (Fishing, Cooking, etc.)" messages. For example, "Requires Mining 500 (currently 496)"
-	1.2 : Added command to print highest herb/mob/mine you can pick/skin/mine, fixed "Requires skinning knife (currently 0)" bug
-	1.3 : Updated for 5.0, added Pandaria resources, now takes into account levels gained from enchants and items in bag (Herbalist's Spade, etc.)
-	1.4 : Added node/herb/skinnable mob tooltip modifications
-	1.41: Fixed bug with tooltips
-	1.42: Fixed the addon not allowing other addons to hide its UIErrorsFrame messages (like Leatrix Plus), thanks tordekflesk
-	1.43: Fixed duplicate "Now able to mine..." messages when you don't have any skill bonuses
-	1.44: Fixed error when hovering over Truesilver deposits
-	1.45: Updated TOC version for 5.1
-	1.46: Updated TOC version for 5.2
-	1.47: Updated TOC version for 5.3, and fixed minor skinning error (need 10 Skinning, not 1, to skin a level 11 mob)
-	1.48: Updated TOC version for 5.4
-]]
-
-local origErrorFunc
-
--- Table format: {level, prettyName, objectName}
--- If prettyName is nil, it's only used for modifying object tooltips, it should not be reported in chat that it is now mineable
--- If objectName is missing, it's assumed that it's the same as prettyName (as in most herbs)
+GN = {}
+GN.origErrorFunc
 
 local mining_levels = {
 	{1,   "Copper", 		"Copper Vein"},
@@ -219,12 +200,128 @@ local skinning_levels = {
 	{560, "90"},
 }
 
-function GNGetHighest(skill,level)
+--- Events ---
+
+function GN.OnLoad(self)						-- Fired when the addon is loaded
+	self:RegisterEvent("CHAT_MSG_SKILL")
+	GameTooltip:HookScript("OnShow",GN.ModifyTooltip)
+	
+	GN.origErrorFunc = UIErrorsFrame:GetScript('OnEvent')
+	UIErrorsFrame:SetScript('OnEvent', GN.OnUIError)
+	
+	SLASH_GN1, SLASH_GN2, SLASH_GN3 = '/gn', '/gathernotify', '/gnotify'
+	SlashCmdList["GN"] = GN.OnCommand
+end
+
+function GN.OnEvent(self, event, ...)			-- Fired on a registered event
+	GN.Debug("event : " .. event)
+	if event == "CHAT_MSG_SKILL" then
+		local skill = strmatch(...,"Your skill in (%a*) has increased to")
+		if skill == "Mining" or skill == "Herbalism" or skill == "Skinning" then
+			local level,temp = GN.GetProfessionLevel(skill)
+			
+			----- First, print if you can mine something new, without taking temp bonuses into account -----
+			local new = GN.GetAllGatherable(skill,level)
+			
+			if new then
+				local tempmsg = ""
+				if temp>0 then tempmsg = " (without current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
+				
+					if skill == "Skinning"	then GN.Msg("Now able to skin level "..new.." creatures"..tempmsg,true)
+				elseif skill == "Mining"	then GN.Msg("Now able to mine "..new..tempmsg,true)
+				elseif skill == "Herbalism"	then GN.Msg("Now able to pick "..new..tempmsg,true)
+				end
+			end
+			----- End -----
+			
+			----- Now, print if you can mine something new, WITH temp bonuses, but only if it differs from without bonuses -----
+			local newtemp = GN.GetAllGatherable(skill,level+temp)
+			
+			if newtemp and newtemp ~= new then
+				local tempmsg = ""
+				if temp>0 then tempmsg = " (with current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
+				
+					if skill == "Skinning"	then GN.Msg("Now able to skin level "..newtemp.." creatures"..tempmsg,true)
+				elseif skill == "Mining"	then GN.Msg("Now able to mine "..newtemp..tempmsg,true)
+				elseif skill == "Herbalism"	then GN.Msg("Now able to pick "..newtemp..tempmsg,true)
+				end
+			end
+			----- End -----
+		end
+	end
+end
+
+function GN.OnCommand(cmd)						-- Fired when a slash command is entered
+	cmd = cmd:lower()
+	
+	if cmd == "s" or cmd == "skinning" then
+		GN.PrintHighestNode("skinning")
+	
+	elseif cmd == "m" or cmd == "mining" then
+		GN.PrintHighestNode("mining")
+	
+	elseif cmd == "h" or cmd == "herbalism" then
+		GN.PrintHighestNode("herbalism")
+	
+	elseif cmd == "v" or cmd == "ver" or cmd == "version" then
+		GN.Msg("GatherNotify version: "..GetAddOnMetadata("GatherNotify","Version"))
+	
+	else
+		GN.Msg("/gn skinning - Print highest level creature you can skin")
+		GN.Msg("/gn mining - Print highest node you can mine")
+		GN.Msg("/gn herbalism - Print highest herb you can pick")
+		GN.Msg("/gn version - Prints addon version")
+	end
+end
+
+function GN.OnUIError(self, event, err, ...)	-- Fired when a red error is shown at the top of the screen
+	local skill = strmatch(err,"Requires (.*) ")
+	local rlevel = tonumber(strmatch(err,"Requires .* (%d+)"))
+	
+	if skill and rlevel then -- Error was a "requires" error
+		local level,temp = GN.GetProfessionLevel(skill)
+		if level then
+			GN.AddError(skill,rlevel,level,temp)
+		else
+			return GN.origErrorFunc(self, event, err, ...)
+		end
+	else
+		return GN.origErrorFunc(self, event, err, ...)
+	end
+end
+
+--- Functions ---
+
+function GN.Msg(msg,noname)						-- Print a message to the chat frame
+	if noname then
+		DEFAULT_CHAT_FRAME:AddMessage("|cffFFC300"..msg)
+	else
+		DEFAULT_CHAT_FRAME:AddMessage("|cffd2b48c[GatherNotify]|r "..msg)
+	end
+end
+
+function GN.Debug(...)							-- Print a debug message to the chat frame
+	--if GN_Settings.debug then
+		local str = table.concat({...}, " ")
+		DEFAULT_CHAT_FRAME:AddMessage("|cffff0000[GN]|r " .. str)
+	--end
+end
+
+function GN.AddError(skill,rlevel,current,temp)	-- Print a modified red error to the top of the screen
+	if temp == 0 then
+		UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s)",skill,rlevel,current),1.0,0.1,0.1,1.0)
+	else
+		--UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s - %s|cff1aff1a+%s|r)",skill,rlevel,current+temp,current,temp),1.0,0.1,0.1,1.0)
+		UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s|cff1aff1a+%s|r)",skill,rlevel,current,temp),1.0,0.1,0.1,1.0)
+	end
+end
+
+function GN.GetHighestNode(skill,level)			-- Get the highest herb/mine/corpse usable at the current skill level
 	local tbl
 		if string.lower(skill) == "skinning" then tbl = skinning_levels
 	elseif string.lower(skill) == "mining" then tbl = mining_levels
 	elseif string.lower(skill) == "herbalism" then tbl = herbalism_levels
-	  else return nil
+	  else return
 	end
 	
 	local highest = 1
@@ -234,10 +331,38 @@ function GNGetHighest(skill,level)
 		end
 	end
 	
-	return GNGetForLevel(skill,highest)
+	return GN.GetAllGatherable(skill,highest)
 end
 
-function GNGetForLevel(skill,level)
+function GN.PrintHighestNode(skill)					-- Print the highest herb/mine/corpse usable at the current skill level
+	local level,temp = GN.GetProfessionLevel(skill)
+	
+	if level then
+		----- First, print highest mineable without taking temp bonuses into account -----
+		local tempmsg = ""
+		if temp>0 and GN.GetHighestNode(skill,level+temp) ~= GN.GetHighestNode(skill,level) then tempmsg = " (without current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
+		if skill == "skinning"	then GN.Msg("Skinning "..level..": level "..GN.GetHighestNode("skinning",level).." creatures"..tempmsg,true) end
+		if skill == "mining" 	then GN.Msg("Mining "..level..": "..GN.GetHighestNode("mining",level)..tempmsg,true) end
+		if skill == "herbalism"	then GN.Msg("Herbalism "..level..": "..GN.GetHighestNode("herbalism",level)..tempmsg,true) end
+		----- End -----
+		
+		----- Then print highest mineable, taking temp bonuses into account -----
+		if found==true then
+			if GN.GetHighestNode(skill,level+temp) ~= GN.GetHighestNode(skill,level) then -- Only print what you can mine with current bonus if it differs from without bonus
+				tempmsg = ""
+				if temp>0 then tempmsg = " (with current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
+				if skill == "skinning"	then GN.Msg("Skinning |cff1aff1a"..level+temp.."|cffFFC300: level "..GN.GetHighestNode("skinning",level+temp).." creatures"..tempmsg,true) end
+				if skill == "mining" 	then GN.Msg("Mining |cff1aff1a"..level+temp.."|cffFFC300: "..GN.GetHighestNode("mining",level+temp)..tempmsg,true) end
+				if skill == "herbalism"	then GN.Msg("Herbalism |cff1aff1a"..level+temp.."|cffFFC300: "..GN.GetHighestNode("herbalism",level+temp)..tempmsg,true) end
+			end
+		----- End -----
+		end
+	else
+		GN.Msg("You don't have "..skill)
+	end
+end
+
+function GN.GetAllGatherable(skill,level)		-- Get a list of all herbs/mines/corpses usable at the current skill level
 	local tbl
 		if string.lower(skill) == "skinning" then tbl = skinning_levels
 	elseif string.lower(skill) == "mining" then tbl = mining_levels
@@ -257,17 +382,17 @@ function GNGetForLevel(skill,level)
 	if #ret == 0 then return nil else return table.concat(ret,", ") end
 end
 
-function GNGetLevelForObject(skill,name)
+function GN.GetRequiredLevel(skill,name)		-- Get the required herbalism/mining/skinning level required to use a node/corpse
 	name = tostring(name)
 	local tbl,ret
 		if string.lower(skill) == "skinning" then tbl = skinning_levels
 	elseif string.lower(skill) == "mining" then tbl = mining_levels
 	elseif string.lower(skill) == "herbalism" then tbl = herbalism_levels
-	  else return nil
+	  else return
 	end
 	
 	for i,v in pairs(tbl) do
-		if (v[3] == nil and v[2] == name) or v[3] == name then
+		if (not v[3] and v[2] == name) or v[3] == name then
 			ret = v[1]
 		end
 	end
@@ -275,7 +400,7 @@ function GNGetLevelForObject(skill,name)
 	return ret
 end
 
-function GNGetProfessionLevel(name)
+function GN.GetProfessionLevel(name)			-- Get a profession's level
 	local p = {}
 	local n,l,t,level,temp
 	p[1],p[2],p[3],p[4],p[5],p[6] = GetProfessions()
@@ -291,12 +416,12 @@ function GNGetProfessionLevel(name)
 	return level,temp
 end
 
-function GNModifyTooltip(self, ...)
+function GN.ModifyTooltip(self, ...)			-- Modify an herb's/mine's/corpse's tooltip
 	if GameTooltipTextLeft2:GetText() == "Requires Mining" then
-		req = GNGetLevelForObject("Mining",GameTooltipTextLeft1:GetText())
+		req = GN.GetRequiredLevel("Mining",GameTooltipTextLeft1:GetText())
 		r,g,b,a = GameTooltipTextLeft2:GetTextColor()
 		if req then GameTooltipTextLeft2:SetText("Requires Mining "..req) end
-		local skill,temp = GNGetProfessionLevel("Mining")
+		local skill,temp = GN.GetProfessionLevel("Mining")
 		if skill then
 			local tempmsg = ""
 			if temp>0 then tempmsg = "|cff1aff1a+"..temp.."|r" end
@@ -305,10 +430,10 @@ function GNModifyTooltip(self, ...)
 		GameTooltip:Show()
 	
 	elseif GameTooltipTextLeft2:GetText() == "Requires Herbalism" then
-		req = GNGetLevelForObject("Herbalism",GameTooltipTextLeft1:GetText())
+		req = GN.GetRequiredLevel("Herbalism",GameTooltipTextLeft1:GetText())
 		r,g,b,a = GameTooltipTextLeft2:GetTextColor()
 		if req then GameTooltipTextLeft2:SetText("Requires Herbalism "..req) end
-		local skill,temp = GNGetProfessionLevel("Herbalism")
+		local skill,temp = GN.GetProfessionLevel("Herbalism")
 		if skill then
 			local tempmsg = ""
 			if temp>0 then tempmsg = "|cff1aff1a+"..temp.."|r" end
@@ -320,10 +445,10 @@ function GNModifyTooltip(self, ...)
 		local sline = 0
 		for i=1,GameTooltip:NumLines() do if _G["GameTooltipTextLeft"..i]:GetText() == "Skinnable" then sline = i end end
 		if sline > 0 then
-			req = GNGetLevelForObject("Skinning",UnitLevel("mouseover"))
+			req = GN.GetRequiredLevel("Skinning",UnitLevel("mouseover"))
 			r,g,b,a = _G["GameTooltipTextLeft"..sline]:GetTextColor()
 			if req then _G["GameTooltipTextLeft"..sline]:SetText("Skinnable ("..req..")") end
-			local skill,temp = GNGetProfessionLevel("Skinning")
+			local skill,temp = GN.GetProfessionLevel("Skinning")
 			if skill then
 				local tempmsg = ""
 				if temp>0 then tempmsg = "|cff1aff1a+"..temp.."|r" end
@@ -334,152 +459,13 @@ function GNModifyTooltip(self, ...)
 	end
 end
 
-function OnUIError(self, event, err, ...)
-	local skill = strmatch(err,"Requires (.*) ")
-	local rlevel = tonumber(strmatch(err,"Requires .* (%d+)"))
-	
-	if (skill ~= nil) and (rlevel ~= nil) then -- Error was a "requires" error
-		local level,temp = GNGetProfessionLevel(skill)
-		if level then
-			GN_AddError(skill,rlevel,level,temp)
-		else
-			return origErrorFunc(self, event, err, ...)
-		end
-	else
-		return origErrorFunc(self, event, err, ...)
-	end
+-- Debug functions
+
+function GN.err(skill,level)
+	GN.OnEvent(nil,"UI_ERROR_MESSAGE",string.format("Requires %s %s",skill,level))
 end
 
-function GN_OnLoad(self)
-	self:RegisterEvent("CHAT_MSG_SKILL")
-	GameTooltip:HookScript("OnShow",GNModifyTooltip)
-	
-	origErrorFunc = UIErrorsFrame:GetScript('OnEvent')
-	UIErrorsFrame:SetScript('OnEvent', OnUIError)
-	
-	--self:RegisterEvent("UI_ERROR_MESSAGE")
-	--UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
-end
-
-function GNTestError(skill,level)
-	GN_OnEvent(nil,"UI_ERROR_MESSAGE",string.format("Requires %s %s",skill,level))
-end
-
-function GN_OnEvent(self, event, ...)
-	if (event == "CHAT_MSG_SKILL") then
-		local skill = strmatch(...,"Your skill in (%a*) has increased to")
-		if skill == "Mining" or skill == "Herbalism" or skill == "Skinning" then
-			local level,temp = GNGetProfessionLevel(skill)
-			
-			----- First, print if you can mine something new, without taking temp bonuses into account -----
-			local new = GNGetForLevel(skill,level)
-			
-			if new then
-				local tempmsg = ""
-				if temp>0 then tempmsg = " (without current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
-				
-					if skill == "Skinning"	then GN_Msg("Now able to skin level "..new.." creatures"..tempmsg,true)
-				elseif skill == "Mining"	then GN_Msg("Now able to mine "..new..tempmsg,true)
-				elseif skill == "Herbalism"	then GN_Msg("Now able to pick "..new..tempmsg,true)
-				end
-			end
-			----- End -----
-			
-			----- Now, print if you can mine something new, WITH temp bonuses, but only if it differs from without bonuses -----
-			local newtemp = GNGetForLevel(skill,level+temp)
-			
-			if newtemp and newtemp ~= new then
-				local tempmsg = ""
-				if temp>0 then tempmsg = " (with current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
-				
-					if skill == "Skinning"	then GN_Msg("Now able to skin level "..newtemp.." creatures"..tempmsg,true)
-				elseif skill == "Mining"	then GN_Msg("Now able to mine "..newtemp..tempmsg,true)
-				elseif skill == "Herbalism"	then GN_Msg("Now able to pick "..newtemp..tempmsg,true)
-				end
-			end
-			----- End -----
-		end
-	
-	-- Using a SetScript based method now for better integration with other addons that modify UIErrorsFrame
-	--[[
-	elseif (event == "UI_ERROR_MESSAGE") then
-		local skill = strmatch(...,"Requires (.*) ")
-		local rlevel = tonumber(strmatch(...,"Requires .* (%d+)"))
-		
-		if ((skill ~= nil) and (rlevel ~= nil)) then -- Error was a "requires" error
-			local level,temp = GNGetProfessionLevel(skill)
-			if level then
-				GN_AddError(skill,rlevel,level,temp)
-			else
-				UIErrorsFrame:AddMessage(...,1,.1,.1)
-			end
-		else
-			UIErrorsFrame:AddMessage(...,1,.1,.1)
-		end
-	]]
-	end
-end
-
-function GN_AddError(skill,rlevel,current,temp)
-	if temp == 0 then
-		UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s)",skill,rlevel,current),1.0,0.1,0.1,1.0)
-	else
-		--UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s - %s|cff1aff1a+%s|r)",skill,rlevel,current+temp,current,temp),1.0,0.1,0.1,1.0)
-		UIErrorsFrame:AddMessage(string.format("Requires %s %s (currently %s|cff1aff1a+%s|r)",skill,rlevel,current,temp),1.0,0.1,0.1,1.0)
-	end
-end
-
-function GN_Msg(msg,noname)
-	if noname then
-		DEFAULT_CHAT_FRAME:AddMessage("|cffFFC300"..msg)
-	else
-		DEFAULT_CHAT_FRAME:AddMessage("|cffd2b48c[GatherNotify]|r "..msg)
-	end
-end
-
-SLASH_GN1, SLASH_GN2, SLASH_GN3 = '/gn', '/gathernotify', '/gnotify'
-function SlashCmdList.GN(cmd)
-	cmd = string.lower(cmd)
-	if (cmd == "s" or cmd == "skinning") then
-		GN_PrintHighest("skinning")
-	elseif (cmd == "m" or cmd == "mining") then
-		GN_PrintHighest("mining")
-	elseif (cmd == "h" or cmd == "herbalism") then
-		GN_PrintHighest("herbalism")
-	elseif (cmd == "v" or cmd == "ver" or cmd == "version") then
-		GN_Msg("GatherNotify version: "..GetAddOnMetadata("GatherNotify","Version"))
-	else
-		GN_Msg("/gn skinning - Print highest level creature you can skin")
-		GN_Msg("/gn mining - Print highest node you can mine")
-		GN_Msg("/gn herbalism - Print highest herb you can pick")
-		GN_Msg("/gn version - Prints addon version")
-	end
-end
-
-function GN_PrintHighest(skill)
-	local level,temp = GNGetProfessionLevel(skill)
-	
-	if level then
-		----- First, print highest mineable without taking temp bonuses into account -----
-		local tempmsg = ""
-		if temp>0 and GNGetHighest(skill,level+temp) ~= GNGetHighest(skill,level) then tempmsg = " (without current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
-		if skill == "skinning"	then GN_Msg("Skinning "..level..": level "..GNGetHighest("skinning",level).." creatures"..tempmsg,true) end
-		if skill == "mining" 	then GN_Msg("Mining "..level..": "..GNGetHighest("mining",level)..tempmsg,true) end
-		if skill == "herbalism"	then GN_Msg("Herbalism "..level..": "..GNGetHighest("herbalism",level)..tempmsg,true) end
-		----- End -----
-		
-		----- Then print highest mineable, taking temp bonuses into account -----
-		if found==true then
-			if GNGetHighest(skill,level+temp) ~= GNGetHighest(skill,level) then -- Only print what you can mine with current bonus if it differs from without bonus
-				tempmsg = ""
-				if temp>0 then tempmsg = " (with current |cff1aff1a+"..temp.."|cffFFC300 bonus)" end
-				if skill == "skinning"	then GN_Msg("Skinning |cff1aff1a"..level+temp.."|cffFFC300: level "..GNGetHighest("skinning",level+temp).." creatures"..tempmsg,true) end
-				if skill == "mining" 	then GN_Msg("Mining |cff1aff1a"..level+temp.."|cffFFC300: "..GNGetHighest("mining",level+temp)..tempmsg,true) end
-				if skill == "herbalism"	then GN_Msg("Herbalism |cff1aff1a"..level+temp.."|cffFFC300: "..GNGetHighest("herbalism",level+temp)..tempmsg,true) end
-			end
-		----- End -----
-		end
-	else
-		GN_Msg("You don't have "..skill)
-	end
-end
+-- Initialize the addon (not using XML)
+local frame = CreateFrame("Frame")
+GN.OnLoad(frame)
+frame:SetScript("OnEvent", GN.OnEvent)
